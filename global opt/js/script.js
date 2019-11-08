9ff17b5703249bd599453c56f7a82645d6381f68
05shams942211 <57525097+05shams942211@users.noreply.github.com>
2019-11-08 12:44:46 +0300

    Add files via upload

---
+++ global opt/js/script.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+    $('.hamburger').on('click', function() {
+        $('.promo_nav').toggleClass('promo_nav_active');
+        $(this).toggleClass('hamburger_active');
+    });
+
+    $('.promo_menu a[href^="#"]').on('click', function() {
+        $('.promo_nav').removeClass('promo_nav_active');
+        $('.hamburger').removeClass('hamburger_active');
+    });
+
+
+    //scroll 
+    $("a[href^='#'").click(function() {
+        var _href = $(this).attr("href");
+        $("html, body").animate({scrollTop:$(_href).offset().top + "px"});
+        return false;
+    });
+
+    
+    //mask phone
+    $('input[name=phone]').mask("+7(999)-999-99-99")
+
+
+    //ajax
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+
+
+            $('form').trigger('reset');
+        });
+        return false;
+    });
+});
